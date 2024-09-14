7d4676312a29e3f795bbc0de667295795b6003ff
Anon Dev <michaelerastus9@gmail.com>
2024-09-14 11:05:04 +0100

    Added Recaptcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="zxx">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -31,140 +32,155 @@
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="{{asset('home/images/'.$web->logo)}}">
     <title>{{$pageName}} - {{$siteName}}</title>
-    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+    </script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
 <body class="body-bg-f5f5f5">
-<!-- Start Preloader Area -->
-<div class="preloader">
-    <div class="content">
-        <div class="box"></div>
-    </div>
-</div>
-<!-- End Preloader Area -->
-
-<!-- Start User Area -->
-<section class="user-area">
-    <div class="container">
-        <div class="user-form-content">
-            <h3>Register</h3>
-            <p>Register to continue to {{$siteName}}.</p>
-
-            <form class="user-form" method="post" action="{{route('auth.register')}}">
-                @include('templates.notification')
-                @csrf
-                <div class="row">
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Name</label>
-                            <input class="form-control" type="text" placeholder="Enter your name"
-                                   value="{{old('name')}}" name="name">
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input class="form-control" type="text" placeholder="Enter your username"
-                                   name="username" value="{{old('username')}}"/>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input class="form-control" type="email"  name="email" value="{{old('email')}}"
-                                   placeholder="Enter your email">
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Phone</label>
-                            <input class="form-control" type="text" placeholder="Enter your Phone"
-                                   name="phone" value="{{old('phone')}}"/>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input class="form-control" type="password" name="password"
-                                   placeholder="Enter your password">
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Repeat password</label>
-                            <input class="form-control" type="password" name="password_confirmation"
-                                   placeholder="Enter your repeat password">
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Referral</label>
-                            <input class="form-control" type="text" placeholder="Enter your Phone"
-                                   name="referral" value="{{old('referral')}} {{$referral}}"/>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <button class="default-btn register" type="submit">
-                            Sign up
-                        </button>
-                    </div>
-
-
-
-                    <div class="col-12">
-                        <p class="create">Already have an account? <a href="{{route('login')}}">Sign in</a></p>
-                    </div>
-                </div>
-            </form>
+    <!-- Start Preloader Area -->
+    <div class="preloader">
+        <div class="content">
+            <div class="box"></div>
         </div>
     </div>
-</section>
-<!-- End User Area -->
+    <!-- End Preloader Area -->
 
-<div class="dark-bar">
-    <a href="#" class="d-flex align-items-center">
-        <span class="dark-title">Enable Dark Theme</span>
-    </a>
+    <!-- Start User Area -->
+    <section class="user-area">
+        <div class="container">
+            <div class="user-form-content">
+                <h3>Register</h3>
+                <p>Register to continue to {{$siteName}}.</p>
 
-    <div class="form-check form-switch">
-        <input type="checkbox" class="checkbox" id="darkSwitch">
+                <form class="user-form" method="post" action="{{route('auth.register')}}">
+                    @include('templates.notification')
+                    @csrf
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input class="form-control" type="text" placeholder="Enter your name"
+                                    value="{{old('name')}}" name="name">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Username</label>
+                                <input class="form-control" type="text" placeholder="Enter your username"
+                                    name="username" value="{{old('username')}}" />
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input class="form-control" type="email" name="email" value="{{old('email')}}"
+                                    placeholder="Enter your email">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Phone</label>
+                                <input class="form-control" type="text" placeholder="Enter your Phone" name="phone"
+                                    value="{{old('phone')}}" />
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input class="form-control" type="password" name="password"
+                                    placeholder="Enter your password">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Repeat password</label>
+                                <input class="form-control" type="password" name="password_confirmation"
+                                    placeholder="Enter your repeat password">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Referral</label>
+                                <input class="form-control" type="text" placeholder="Enter your Phone" name="referral"
+                                    value="{{old('referral')}} {{$referral}}" />
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <strong>ReCaptcha:</strong>
+                                    <div class="g-recaptcha" data-sitekey="{{ env('GOOGLE_RECAPTCHA_KEY') }}"></div>
+                                    @if ($errors->has('g-recaptcha-response'))
+                                    <span class="text-danger">{{ $errors->first('g-recaptcha-response') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <button class="default-btn register" type="submit">
+                                Sign up
+                            </button>
+                        </div>
+
+
+
+                        <div class="col-12">
+                            <p class="create">Already have an account? <a href="{{route('login')}}">Sign in</a></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <!-- End User Area -->
+
+    <div class="dark-bar">
+        <a href="#" class="d-flex align-items-center">
+            <span class="dark-title">Enable Dark Theme</span>
+        </a>
+
+        <div class="form-check form-switch">
+            <input type="checkbox" class="checkbox" id="darkSwitch">
+        </div>
     </div>
-</div>
 
-<!-- Start Go Top Area -->
-<div class="go-top">
-    <i class="ri-arrow-up-s-fill"></i>
-    <i class="ri-arrow-up-s-fill"></i>
-</div>
-<!-- End Go Top Area -->
+    <!-- Start Go Top Area -->
+    <div class="go-top">
+        <i class="ri-arrow-up-s-fill"></i>
+        <i class="ri-arrow-up-s-fill"></i>
+    </div>
+    <!-- End Go Top Area -->
 
-<!-- Jquery Min JS -->
-<script src="{{asset('dashboard/user/js/jquery.min.js')}}"></script>
-<!-- Bootstrap Bundle Min JS -->
-<script src="{{asset('dashboard/user/js/bootstrap.bundle.min.js')}}"></script>
-<!-- Owl Carousel Min JS -->
-<script src="{{asset('dashboard/user/js/owl.carousel.min.js')}}"></script>
-<!-- Metismenu Min JS -->
-<script src="{{asset('dashboard/user/js/metismenu.min.js')}}"></script>
-<!-- Simplebar Min JS -->
-<script src="{{asset('dashboard/user/js/simplebar.min.js')}}"></script>
-<!-- mixitup Min JS -->
-<script src="{{asset('dashboard/user/js/mixitup.min.js')}}"></script>
-<!-- Dark Mode Switch Min JS -->
-<script src="{{asset('dashboard/user/js/dark-mode-switch.min.js')}}"></script>
-<!-- Form Validator Min JS -->
-<script src="{{asset('dashboard/user/js/form-validator.min.js')}}"></script>
-<!-- Contact JS -->
-<script src="{{asset('dashboard/user/js/contact-form-script.js')}}"></script>
-<!-- Ajaxchimp Min JS -->
-<script src="{{asset('dashboard/user/js/ajaxchimp.min.js')}}"></script>
-<!-- Custom JS -->
-<script src="{{asset('dashboard/user/js/custom.js')}}"></script>
+    <!-- Jquery Min JS -->
+    <script src="{{asset('dashboard/user/js/jquery.min.js')}}"></script>
+    <!-- Bootstrap Bundle Min JS -->
+    <script src="{{asset('dashboard/user/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- Owl Carousel Min JS -->
+    <script src="{{asset('dashboard/user/js/owl.carousel.min.js')}}"></script>
+    <!-- Metismenu Min JS -->
+    <script src="{{asset('dashboard/user/js/metismenu.min.js')}}"></script>
+    <!-- Simplebar Min JS -->
+    <script src="{{asset('dashboard/user/js/simplebar.min.js')}}"></script>
+    <!-- mixitup Min JS -->
+    <script src="{{asset('dashboard/user/js/mixitup.min.js')}}"></script>
+    <!-- Dark Mode Switch Min JS -->
+    <script src="{{asset('dashboard/user/js/dark-mode-switch.min.js')}}"></script>
+    <!-- Form Validator Min JS -->
+    <script src="{{asset('dashboard/user/js/form-validator.min.js')}}"></script>
+    <!-- Contact JS -->
+    <script src="{{asset('dashboard/user/js/contact-form-script.js')}}"></script>
+    <!-- Ajaxchimp Min JS -->
+    <script src="{{asset('dashboard/user/js/ajaxchimp.min.js')}}"></script>
+    <!-- Custom JS -->
+    <script src="{{asset('dashboard/user/js/custom.js')}}"></script>
 </body>
+
 </html>
